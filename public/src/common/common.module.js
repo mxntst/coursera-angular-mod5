cef1ca37c593854c17b62f5d2f1b9969068e3c73
Maxim <mnovoseltsev@gmail.com>
2018-08-13 09:12:21 +0200

    API Path to glacial-wildwood-60583

--- public/src/common/common.module.js
+++ public/src/common/common.module.js
@@ -2,7 +2,7 @@
 "use strict";
 
 angular.module('common', [])
-.constant('ApiPath', 'https://www.davidchuschinabistro.com')
+.constant('ApiPath', 'https://glacial-wildwood-60583.herokuapp.com/')
 .config(config);
 
 config.$inject = ['$httpProvider'];
