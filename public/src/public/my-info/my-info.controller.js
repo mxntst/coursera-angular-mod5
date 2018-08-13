768e25514560af7ac9e658d0c48b4cf70b066499
Maxim <mnovoseltsev@gmail.com>
2018-08-13 16:32:22 +0200

    menu item, styling

--- public/src/public/my-info/my-info.controller.js
+++ public/src/public/my-info/my-info.controller.js
@@ -9,36 +9,16 @@
       var $ctrl = this;
       $ctrl.reg = SignUpService.retrieve();
       if ($ctrl.reg) {
-          $ctrl.title = "Your Account Settings"
+          $ctrl.title = "Your Account Settings";
+          MenuService.getMenuItem( $ctrl.reg.favMenuItem )
+            .then(function (menuItem) {
+          $ctrl.reg.menuItem = menuItem;
+      });
+      console.log($ctrl.reg);
       } else {
         $ctrl.title = "Sign Up"
       }
-      $ctrl.store = function() {
-          //check menu item
-          var regMenuItem = $ctrl.reg.favMenuItem;
-          MenuService.getMenuItem(regMenuItem)
-          .then(function(menuItem) {
-              if (menuItem) {
-                try {
-                    SignUpService.store($ctrl.reg);
-                    $ctrl.saved = true;
-                } catch (e) {
-                    console.error("local storage fails", e);
-                    $ctrl.error = "Can not store data, internal server error";
-                }
-               
-                $ctrl.error = null;
-              } else {
-                  console.warn("menu item is incorrect", regMenuItem);
-                  $ctrl.error = "No such menu number exist";
-              }
-          })
-          .catch(function(e) {
-            console.error("catch in SignUp controller", e);
-            $ctrl.error = "No such menu number exist";
-          });
-          
-      }
+             
     }
     
     })();
